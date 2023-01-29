16d748348c28bcfdf653a589b15568775920c58b
RF0606 <15rf17@queensu.ca>
2023-01-29 18:16:32 -0500

    create the init structure

---
+++ mall-ware/src/test/java/com/peter/mall/lware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.mall.lware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
