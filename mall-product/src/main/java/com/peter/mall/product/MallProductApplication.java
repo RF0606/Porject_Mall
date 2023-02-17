36536abb3fa4856f1b3e8cd136bb50e12373ab65
RF0606 <15rf17@queensu.ca>
2023-02-17 00:43:15 -0500

    Reverse Engineering

--- mall-product/src/main/java/com/peter/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/peter/mall/product/MallProductApplication.java
@@ -1,8 +1,10 @@
 package com.peter.mall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.peter.mall.product.dao")
 @SpringBootApplication
 public class MallProductApplication {
 
--- mall-product/src/test/java/com/peter/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/peter/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,40 @@
 package com.peter.mall.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.peter.mall.product.entity.BrandEntity;
+import com.peter.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+
+//        BrandEntity brandEntity = new BrandEntity();
+
+//        brandEntity.setDescript("test");
+//        brandEntity.setName("huawei");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功。。。");
+
+//        brandEntity.setBrandId(1L);
+//        brandEntity.setDescript("test2222");
+//
+//        brandService.updateById(brandEntity);
+//        System.out.println("修改成功");
+
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+        list.forEach((item)->{
+            System.out.println(item);
+        });
     }
 
 }
